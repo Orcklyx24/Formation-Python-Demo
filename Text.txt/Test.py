2571b22154ca124d19d21d983c212febcea14cf7
Orcklyx24 <orcklyxanato@gmail.com>
2025-01-23 21:11:46 +0100

    initialisation du projet test

---
+++ Text.txt/Test.py
@@ -0,0 +1,21 @@
+x = 5       
+print("Ici nous avons déclaré une variable *x* qui prend la valeur 5")
+
+# Opérations arithmétiques
+addition = 5 + 3
+soustraction = 5 - 3
+multiplication = 5 * 3
+division = 5 / 3
+modulo = 5 % 3
+puissance = 5 ** 3
+
+print(f"Cette addition est égale à : {addition}")
+print(f"Cette addition est égale à : {soustraction}")
+print(f"Cette addition est égale à : {multiplication}")
+print(f"Cette addition est égale à : {division}")
+print(f"Cette addition est égale à : {modulo}")
+print(f"Cette addition est égale à : {puissance}")
+
+# Qui suis je
+# Je suis Tchchnicien Travaux Statistiques et Data Scientist
+Test.txt
